02bbbbc8307d8c93bc1b160b1febcb156e1c05e4
Friederich <siyfred1n@gmail.com>
2025-06-11 10:23:22 -0300

    initial setup

---
+++ src/main/java/com/siyfred/roadtrack/RoadtrackApplication.java
@@ -0,0 +1,13 @@
+package com.siyfred.roadtrack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoadtrackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoadtrackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/siyfred/roadtrack/RoadtrackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.siyfred.roadtrack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoadtrackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
